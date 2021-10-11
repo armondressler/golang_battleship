843fb0801ef9b864deb99ae31571eee7e06ec37d
tetanushamster <armon.dressler@gmail.com>
2021-10-12 00:52:45 +0200

    -

--- game/game.go
+++ game/game.go
@@ -1,1 +1,21 @@
 package game
+
+import (
+	"golang_battleship/board"
+	"golang_battleship/player"
+	"golang_battleship/ship"
+
+	"github.com/google/uuid"
+)
+
+type game struct {
+	participants []participant
+	id           uuid.UUID
+}
+
+type participant struct {
+	player player.Player
+	ship   []ship.Ship
+	board  board.Board
+	id     uuid.UUID
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module golang_battleship
 
 go 1.17
+
+require (
+	github.com/alecthomas/kong v0.2.17
+	github.com/google/uuid v1.3.0
+)
