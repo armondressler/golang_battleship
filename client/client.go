3995131d385c373624298859d20a817d83077b77
armondressler <armon.dressler@gmail.com>
2022-01-27 01:30:10 +0100

    websocket shorthand

--- client/client.go
+++ client/client.go
@@ -7,10 +7,10 @@ import (
 	"os/signal"
 	"time"
 
-	"github.com/gorilla/websocket"
+	ws "github.com/gorilla/websocket"
 )
 
-func Connect(addr string, port int, loglevel int) {
+func Connect(addr string, port int) {
 
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
@@ -18,7 +18,7 @@ func Connect(addr string, port int, loglevel int) {
 	u := url.URL{Scheme: "ws", Host: addr, Path: "/echo"}
 	log.Printf("connecting to %s", u.String())
 
-	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	c, _, err := ws.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
 		log.Fatal("dial:", err)
 	}
@@ -46,7 +46,7 @@ func Connect(addr string, port int, loglevel int) {
 		case <-done:
 			return
 		case t := <-ticker.C:
-			err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
+			err := c.WriteMessage(ws.TextMessage, []byte(t.String()))
 			if err != nil {
 				log.Println("write:", err)
 				return
@@ -56,7 +56,7 @@ func Connect(addr string, port int, loglevel int) {
 
 			// Cleanly close the connection by sending a close message and then
 			// waiting (with timeout) for the server to close the connection.
-			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			err := c.WriteMessage(ws.CloseMessage, ws.FormatCloseMessage(ws.CloseNormalClosure, ""))
 			if err != nil {
 				log.Println("write close:", err)
 				return
