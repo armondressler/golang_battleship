848150d00fa75e12a207ac05e2e1718ec9418f3f
armondressler <armon.dressler@gmail.com>
2022-02-23 02:18:22 +0100

    extended list_games test

--- api/api_test.go
+++ api/api_test.go
@@ -1,8 +1,10 @@
 package api
 
 import (
+	"encoding/json"
 	"golang_battleship/game"
 	"golang_battleship/player"
+	"io/ioutil"
 	"net/http"
 	"testing"
 	"time"
@@ -14,7 +16,7 @@ import (
 func TestRegister(t *testing.T) {
 	finish := make(chan struct{})
 	r := mux.NewRouter()
-	r.HandleFunc("/home", Scoreboard)
+	r.HandleFunc("/players", Scoreboard)
 	r.HandleFunc("/register", RegisterPlayer)
 
 	go func() {
@@ -62,7 +64,7 @@ func TestRegister(t *testing.T) {
 
 		apitest.New().
 			EnableNetworking(cli).
-			Get("http://127.0.0.1:8080/home").
+			Get("http://127.0.0.1:8080/players").
 			Expect(t).
 			//Body(`["Rudolf","Dagobert"]`).
 			Status(http.StatusOK).
@@ -78,7 +80,7 @@ func TestListGames(t *testing.T) {
 	r.HandleFunc("/games", ListGames)
 	player.NewPlayer("Rudolf", "")
 	player.NewPlayer("Dagobert", "")
-	game.NewGame(12, 12, 6, "New Game", 2, "Rudolf", "Dagobert")
+	g, _ := game.NewGame(12, 12, 6, "New Game", 2, "Rudolf", "Dagobert")
 
 	go func() {
 		if err := http.ListenAndServe("127.0.0.1:8080", r); err != nil {
@@ -91,13 +93,45 @@ func TestListGames(t *testing.T) {
 			Timeout: time.Second * 1,
 		}
 
-		apitest.New().
+		r := apitest.New().
 			EnableNetworking(cli).
 			Get("http://127.0.0.1:8080/games").
 			Expect(t).
 			Status(http.StatusOK).
 			End()
 
+		body, _ := ioutil.ReadAll(r.Response.Body)
+		var json_body map[string]json.RawMessage
+		var json_body_game map[string]json.RawMessage
+		err := json.Unmarshal(body, &json_body)
+		if err != nil {
+			t.Errorf("failed to find games in json response dict")
+			t.Fail()
+		}
+		err = json.Unmarshal(json_body[g.ID.String()], &json_body_game)
+		if err != nil {
+			t.Errorf("failed to find game id %s in json response dict", g.ID.String())
+			t.Fail()
+		}
+		var json_body_game_participants []string
+		err = json.Unmarshal(json_body_game["participants"], &json_body_game_participants)
+		if err != nil {
+			t.Errorf("failed to find key participants in game with id %s of json response dict", g.ID.String())
+			t.Fail()
+		}
+		for _, p := range json_body_game_participants {
+			found := 0
+			for _, pn := range g.Participants {
+				if pn.String() == p {
+					found = 1
+					break
+				}
+			}
+			if found == 0 {
+				t.Errorf("got unknown participant %s", p)
+				t.Fail()
+			}
+		}
 		finish <- struct{}{}
 	}()
 	<-finish
