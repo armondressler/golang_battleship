ee03dcfe641e8aa52c189f89a8d81735fb6ec50f
armondressler <armon.dressler@gmail.com>
2022-03-31 00:45:10 +0200

    refresh button

--- static/js/gamelist.js
+++ static/js/gamelist.js
@@ -49,11 +49,19 @@ export default {
         },
     },
     template: `
+    <div class="d-flex flex-row">
     <ul class="nav nav-pills my-3">
       <li class="nav-item" v-for="(navItem, index) in navItems" :key="index">
         <a class="nav-link" @click="this.activeNavIndex = index; fetchData()" v-bind:class="{ active: index == this.activeNavIndex }" href="javascript:void(0);">{{ navItem.text }}</a>
-      </li>
+      </li>   
     </ul>
+    <button class="btn btn-outline-secondary ms-auto my-3" type="button" @click="fetchData()">
+    <svg fill="currentColor" width="32" height="32" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+      <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+    </svg>
+    </button>
+    </div>
     <div v-if="loading">
       <strong>
         Loading...
